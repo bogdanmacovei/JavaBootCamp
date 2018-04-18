9b6d2fcd07f893232e31c04eff7b46a38d8af383
Bogdan Macovei <bogdanmaco@yahoo.com>
2018-04-18 20:17:31 +0300

    4th version of the project

--- src/org/bootcamp/MainApp.java
+++ src/org/bootcamp/MainApp.java
@@ -1,11 +1,21 @@
 package org.bootcamp;
 
+import org.bootcamp.calculator.InsurancePolicyCalculator;
+import org.bootcamp.formula.BusBasicFormula;
+import org.bootcamp.formula.CarBasicFormula;
+import org.bootcamp.formula.Formula;
+import org.bootcamp.formula.TipperBasicFormula;
+import org.bootcamp.vehicle.Bus;
+import org.bootcamp.vehicle.Car;
+import org.bootcamp.vehicle.Tipper;
+import org.bootcamp.vehicle.Vehicle;
+
 public class MainApp {
 
     public static void main(String[] args) {
         final Vehicle joesCar = new Car(5, 200000, true, "auto");
-        final Vehicle stevesBus = new Bus (3, 100000, true, 31);
-        final Vehicle petersTipper = new Tipper (6, 80000, false, 15);
+        final Vehicle stevesBus = new Bus(3, 100000, true, 31);
+        final Vehicle petersTipper = new Tipper(6, 80000, false, 15);
 
         final Formula carBasicFormula = new CarBasicFormula();
         final Formula busBasicFormula = new BusBasicFormula();
