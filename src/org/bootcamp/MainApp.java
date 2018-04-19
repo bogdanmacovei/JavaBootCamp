d347163311af37e99204f0155bcec8b1757adaec
Bogdan Macovei <bogdanmaco@yahoo.com>
2018-04-19 18:06:19 +0300

    clean code and 5th version of the project

--- src/org/bootcamp/MainApp.java
+++ src/org/bootcamp/MainApp.java
@@ -1,10 +1,7 @@
 package org.bootcamp;
 
 import org.bootcamp.calculator.InsurancePolicyCalculator;
-import org.bootcamp.formula.BusBasicFormula;
-import org.bootcamp.formula.CarBasicFormula;
-import org.bootcamp.formula.Formula;
-import org.bootcamp.formula.TipperBasicFormula;
+import org.bootcamp.formula.*;
 import org.bootcamp.vehicle.Bus;
 import org.bootcamp.vehicle.Car;
 import org.bootcamp.vehicle.Tipper;
@@ -17,14 +14,10 @@ public class MainApp {
         final Vehicle stevesBus = new Bus(3, 100000, true, 31);
         final Vehicle petersTipper = new Tipper(6, 80000, false, 15);
 
-        final Formula carBasicFormula = new CarBasicFormula();
-        final Formula busBasicFormula = new BusBasicFormula();
-        final Formula tipperBasicFormula = new TipperBasicFormula();
-
         final InsurancePolicyCalculator calculator = InsurancePolicyCalculator.INSTANCE;
-        final int joesInsurancePolicyCost = calculator.calculate(joesCar, carBasicFormula);
-        final int stevesInsurancePolicyCost = calculator.calculate(stevesBus, busBasicFormula);
-        final int petersInsurancePolicyCost = calculator.calculate(petersTipper, tipperBasicFormula);
+        final int joesInsurancePolicyCost = calculator.calculate(joesCar, Formula.CAR_BASIC_FORMULA);
+        final int stevesInsurancePolicyCost = calculator.calculate(stevesBus, Formula.BUS_BASIC_FORMULA);
+        final int petersInsurancePolicyCost = calculator.calculate(petersTipper, Formula.TIPPER_BASIC_FORMULA);
 
         System.out.println("Joe's policy cost is " + joesInsurancePolicyCost);
         System.out.println("Steve's policy cost is " + stevesInsurancePolicyCost);
--- src/org/bootcamp/formula/Formula.java
+++ src/org/bootcamp/formula/Formula.java
@@ -2,6 +2,46 @@ package org.bootcamp.formula;
 
 import org.bootcamp.vehicle.Vehicle;
 
-public interface Formula {
-    int calculate(Vehicle vehicle);
+public enum Formula {
+    CAR_BASIC_FORMULA {
+        @Override
+        public int calculate(Vehicle vehicle) {
+            int cost = 100 * vehicle.getAge();
+            cost += (vehicle.isDiesel()) ? 500 : 0;
+            cost += (vehicle.getNumberOfMiles() > 200000) ? 500 : 0;
+            return cost;
+        }
+    },
+
+    BUS_BASIC_FORMULA {
+        @Override
+        public int calculate(Vehicle vehicle) {
+            int cost = 200 * vehicle.getAge();
+            cost += (vehicle.isDiesel()) ? 1000 : 0;
+
+            if (vehicle.getNumberOfMiles() > 100000 && vehicle.getNumberOfMiles() <= 200000) {
+                cost += 500;
+            } else if (vehicle.getNumberOfMiles() > 200000) {
+                cost += 1000;
+            }
+
+            return cost;
+        }
+    },
+
+    TIPPER_BASIC_FORMULA {
+        @Override
+        public int calculate(Vehicle vehicle) {
+            int cost = 300 * vehicle.getAge();
+            cost += (vehicle.getNumberOfMiles() > 80000) ? 700 : 0;
+
+            return cost;
+        }
+    };
+
+    Formula() {
+
+    }
+
+    public abstract int calculate(Vehicle vehicle);
 }
